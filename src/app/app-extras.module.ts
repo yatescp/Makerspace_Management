b15536a22e92a84be9cc7471247a56b2fe6ded56
yatescp <yatescp@g.cofc.edu>
2020-02-27 16:00:34 -0500

    Added Angular Calendar

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -10,6 +10,28 @@ import {
   SkyAuthHttpModule
 } from '@skyux/http';
 
+import { 
+  BrowserAnimationsModule 
+} from '@angular/platform-browser/animations';
+
+import { 
+  CalendarModule, DateAdapter 
+} from 'angular-calendar';
+
+import { 
+  adapterFactory 
+} from 'angular-calendar/date-adapters/date-fns';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
+  ]
+})
+
 @NgModule({
   exports: [
     AppSkyModule,

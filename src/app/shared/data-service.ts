49ffb37b48491397be4a97a40abcef5fc93637c0
Steven Draugel <steven.draugel@blackbaud.com>
2020-02-28 14:45:37 -0500

    testing different cals

---
+++ src/app/shared/data-service.ts
@@ -0,0 +1,19 @@
+// import { SkyAuthHttp } from '@skyux/http';
+// import { Injectable } from '@angular/core';
+
+// @Injectable()
+// export class DataService {
+
+//   constructor(private http: SkyAuthHttp) {
+
+//   }
+// import { SkyAuthHttp } from '@skyux/http';
+// import { Injectable } from '@angular/core';
+
+// @Injectable()
+// export class DataService {
+
+//   constructor(private http: SkyAuthHttp) {
+
+//   }
+// }
